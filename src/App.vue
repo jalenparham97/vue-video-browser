161170e35caa0095ee58e7e170a46cc693fb23e8
jalen <jalenparham97@gmail.com>
2018-10-03 16:16:36 -0400

    set up youtube api and got data to display

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-    <h1>Hi there!</h1>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoDetail :video="selectedVideo"/>
+    <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+const API_KEY = "AIzaSyAoMVn37vO_neC1SbSr_EKSUfMHM-hMY_M";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null
+    }
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios.get("https://www.googleapis.com/youtube/v3/search", {
+        params: {
+          key: API_KEY,
+          type: "video",
+          part: "snippet",
+          q: searchTerm
+        }
+      }).then(response => this.videos = response.data.items);
+    },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    }
+  }
 }
 </script>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <li class="list-group-item media" @click="onVideoSelect">
+    <img class="mr-3" :src="thumbnailUrl" alt="Video Thumbnail Image">
+    <div class="media-body">{{ title }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "VideoListItem",
+  props: {
+    video: Object
+  },
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+    title() {
+      return this.video.snippet.title;
+    }
+  },
+  methods: {
+    onVideoSelect() {
+      this.$emit("videoSelect", this.video);
+    }
+  } 
+}
+</script>
+
+<style scoped>
+  li {
+    cursor: pointer;
+    display: flex;
+  }
+
+  li:hover {
+    background: #eee;
+  }
+</style>
+
