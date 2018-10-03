184dca51e6f597991e41f1553e1628b1685c8eba
jalen <jalenparham97@gmail.com>
2018-10-03 17:22:06 -0400

    youtube lookalike

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-if="video">
-    {{ video.snippet.title }}  
+  <div v-if="video" class="col-md-8">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe :src="videoUrl" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>  
+    </div>
   </div>
 </template>
 
@@ -9,12 +15,23 @@ export default {
   name: "VideoDetail",
   props: {
     video: Object
+  },
+  computed: {
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .details {
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
 </style>
 
 
